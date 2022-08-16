0c7065a87d40ef961b0b218817ab6cb16443d085
yamaji <haruyuki.program@gmail.com>
2022-08-16 15:32:20 +0900

    一旦コミット

--- public/pages/counter/counter.js
+++ public/pages/counter/counter.js
@@ -1,6 +1,6 @@
 export function counterInit() {
   console.log('counterInit')
-  window.Alpine.data('counter', () => ({
+  Alpine.data('counter', () => ({
     count: 0,
     increment() {
       this.count++;
